9d92e4428440af95e630cbfff419c41a61418466
Mickael Riga <mickael.riga.79@gmail.com>
2022-07-28 07:39:10 +0200

    First commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script>
+
+  import { Pagination } from "swiper";
+  import { Swiper, SwiperSlide } from 'swiper/svelte';
+  import 'swiper/scss';
+  import 'swiper/scss/pagination';
+
+  let slides = [
+    {
+      src: 'https://www.fillmurray.com/200/125/',
+    },
+    {
+      src: 'https://www.fillmurray.com/200/125/',
+    },
+    {
+      src: 'https://www.fillmurray.com/200/125/',
+    },
+  ];
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<Swiper
+  modules={ [ Pagination ] }
+  autoHeight={ true }
+  pagination={{ clickable: true }}
+>
+  {#each slides as slide }
+    <SwiperSlide>
+      <img src={ slide.src } alt='' />
+    </SwiperSlide>
+  {/each}
+</Swiper>
+
